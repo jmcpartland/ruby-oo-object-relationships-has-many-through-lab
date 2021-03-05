72a04058350a418ea3f6c113167cb8f0e05a5f44
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-03-05 07:52:01 -0700

    commit tree 8dca909596649f1f133c8e57724cf273e7937f43

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,25 @@
+class Artist
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select {|s| s.artist == self}
+    end
+
+    def genres
+        Song.all.map {|g| g.genre}
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,24 @@
+class Genre
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|g| g.genre}
+    end
+
+    def artists
+        songs.map {|s| s.artist}
+    end
+
+end
 
